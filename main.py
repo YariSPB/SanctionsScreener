def88d2f278dfa872833fd20c057ce91cb7e6385
YarPas <y.paslavskiy@gmail.com>
2022-05-02 16:27:11 +1100

    ftp download

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import ftplib
+import pathlib
+import os
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -10,9 +12,13 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
     HOSTNAME = "ofacftp.treas.gov"
-    COMMAND_NAME = "/fac_delim"
+    FOLDER_NAME = "/fac_delim"
     USERNAME = "anonymous"
     PASSWORD = "anonymous@domain.com"
+    curr_dir = os.getcwd()
+    raw_data_dir = '/raw_data'
+
+    pathlib.Path(curr_dir + raw_data_dir).mkdir(exist_ok=True)
 
     # Connect FTP Server
     ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
@@ -20,7 +26,20 @@ if __name__ == '__main__':
     # force UTF-8 encoding
     ftp_server.encoding = "utf-8"
 
+
+
+    ftp_server.cwd(FOLDER_NAME)
+
     ftp_server.dir()
+
+    # Enter File Name with Extension
+    filename = "add.csv"
+
+    # Write file in binary mode
+    with open(curr_dir + raw_data_dir +"/"+filename, "wb") as file:
+        # Command for Downloading the file "RETR filename"
+       ftp_server.retrbinary(f"RETR {filename}", file.write)
+
 
     # Close the Connection
     ftp_server.quit()
