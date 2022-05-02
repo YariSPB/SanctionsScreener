2f780359ce21cb144e4a80fe2ea92c8c204e352c
YarPas <y.paslavskiy@gmail.com>
2022-05-02 16:03:35 +1100

    ftp connect

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import ftplib
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +8,21 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+    HOSTNAME = "ofacftp.treas.gov"
+    COMMAND_NAME = "/fac_delim"
+    USERNAME = "anonymous"
+    PASSWORD = "anonymous@domain.com"
+
+    # Connect FTP Server
+    ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
+
+    # force UTF-8 encoding
+    ftp_server.encoding = "utf-8"
+
+    ftp_server.dir()
+
+    # Close the Connection
+    ftp_server.quit()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
